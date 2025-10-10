b3fb83153a94775cdfdc92eec1b84a8ef6553205
shehmir hussain <shahmir.hussain.khanzada@gmail.com>
2025-10-10 19:24:55 +0500

    feat: additional locale (wip)

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -108,7 +108,7 @@ export interface Config {
     header: HeaderSelect<false> | HeaderSelect<true>;
     footer: FooterSelect<false> | FooterSelect<true>;
   };
-  locale: 'de' | 'en';
+  locale: 'de' | 'en' | 'fr';
   user: User & {
     collection: 'users';
   };
